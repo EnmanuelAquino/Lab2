b78d7ce3739845286e6e65bab50569253ccd4698
EnmanuelAquino <126352152+EnmanuelAquino@users.noreply.github.com>
2023-04-22 16:52:24 -0400

    Update test.sh

--- part3/test.sh
+++ part3/test.sh
@@ -3,8 +3,7 @@ source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part3/output
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part3/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal nyc_parking_violations_data.csv /part3/input/
-
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../lab2_data/nyc_parking_violations_data.csv /part3/input/
 
 echo "--------------------------- Parallelism 2---------------------------" 
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 hour_car.py hdfs://$SPARK_MASTER:9000/part3/input/ --conf spark.default.parallelism=2
